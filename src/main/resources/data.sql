1d702fd06f496590828528b6c7409701b66d8654
BobbyBrogrammer <robin.lindholm111@gmail.com>
2026-05-15 11:20:27 +0200

    (V20-Delmål 8) Added: Before-Redis benchmark documentation, also added data.sql file

---
+++ src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO author (name) VALUES ('J.K. Rowling');
+INSERT INTO author (name) VALUES ('J.R.R. Tolkien');
+INSERT INTO author (name) VALUES ('George Orwell');
+INSERT INTO author (name) VALUES ('Agatha Christie');
+INSERT INTO author (name) VALUES ('Stephen King');
+
+INSERT INTO book (title, author_id, isbn, published_year, version) VALUES ('Harry Potter and the Philosopher''s Stone', 1, '978-0-7475-3269-9', 1997, 0);
+INSERT INTO book (title, author_id, isbn, published_year, version) VALUES ('Harry Potter and the Chamber of Secrets', 1, '978-0-7475-3849-3', 1998, 0);
+INSERT INTO book (title, author_id, isbn, published_year, version) VALUES ('The Hobbit', 2, '978-0-261-10221-7', 1937, 0);
+INSERT INTO book (title, author_id, isbn, published_year, version) VALUES ('The Lord of the Rings', 2, '978-0-261-10235-4', 1954, 0);
+INSERT INTO book (title, author_id, isbn, published_year, version) VALUES ('1984', 3, '978-0-452-28423-4', 1949, 0);
+INSERT INTO book (title, author_id, isbn, published_year, version) VALUES ('Animal Farm', 3, '978-0-452-28424-1', 1945, 0);
+INSERT INTO book (title, author_id, isbn, published_year, version) VALUES ('Murder on the Orient Express', 4, '978-0-00-712198-4', 1934, 0);
+INSERT INTO book (title, author_id, isbn, published_year, version) VALUES ('The Shining', 5, '978-0-385-12167-5', 1977, 0);
